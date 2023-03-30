7dbed07023d0e20fbdd63174d7fd7297dcc9d697
Kyle VanderBeek <kylev@kylev.com>
2023-03-29 18:19:49 -0700

    Essential discovery tests.

--- tests/light_test.py
+++ tests/light_test.py
@@ -1,6 +1,17 @@
 import pytest
 
-from ..light import pick_light
+from .. import light
 
-def test_mytest():
-    assert pick_light({}) is None
+from .fixtures import bulbs
+
+
+def test_pick_light_default():
+    assert light.pick_light({}) is None
+
+
+def test_pick_light_color_wifi():
+    assert light.pick_light(bulbs.BULB_W21N13) is light.ElementsColorLightEntity
+
+
+def test_pick_light_white_wifi():
+    assert light.pick_light(bulbs.BULB_W21N11) is light.ElementsLightEntity
